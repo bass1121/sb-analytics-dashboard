4029c8902a48465bcfc710f27cd1e5d4609da243
seanbass <seanbass99@gmail.com>
2021-03-20 15:43:38 -0500

    changed variable name, and added in personal api_token

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -2,8 +2,8 @@ import React, { useEffect, useState } from "react";
 import axios from "axios";
 import ActivityChart from "./ActivityChart";
 
-const API_TOKEN = "211e3ba5-273a-489c-a376-691c68db7527";
-const NAME = "Jordan Hudgens";
+const API_TOKEN = "81c39cf4-ccaa-4c00-a130-326adf6e435c";
+const NAME = "Sean Bass";
 const ENDPOINT = "https://devcamp.com/api/metrics/code_editor_grouped_by_day";
 
 interface IActivityElement {
